1b6a2c8a3c1b249012eb40e92c156d8e9957ec95
juliuskolosnjaji <82638084+juliuskolosnjaji@users.noreply.github.com>
2023-08-17 12:46:58 +0200

    update

--- Bäume/BinaryTree/Main.java
+++ Bäume/BinaryTree/Main.java
@@ -17,17 +17,30 @@ public class Main extends JPanel {
 
     public void setPoint(int x, int y, Graphics g){
         g.setColor(Color.red);
-        g.fillRect(x, y, 5, 5);
+        g.fillRect(x, y, 10, 10);
     }
 
     public void setLine(Graphics g, int x1, int y1, int x2, int y2){
         g.drawLine(x1, y1, x2, y2);
     }
 
+    public static void drawTree(BinaryTree<String> pTree, JFrame f, int xBound, int yBound){
+        JLabel content = new JLabel(pTree.getContent());
+        content.setBounds(xBound - 50, yBound, xBound + 50, yBound + 50);
+        if(pTree.getLeftTree().getContent() != null){
+            drawTree(pTree.getLeftTree(), f, xBound - 100 , yBound + 50);
+        }
+
+        if(pTree.getRightTree().getContent() != null){
+            drawTree(pTree.getRightTree(), f, xBound + 100, yBound + 50);
+        }
+        f.add(content);
+    }
+
     public static void main(String[] args) {
 
         //Baum 
-        BinaryTree links = new BinaryTree<String>("Amogus", null, null);
+        BinaryTree links = new BinaryTree<String>("Amogus", new BinaryTree<String>("Lul"), new BinaryTree<String>("moin"));
         BinaryTree rechts = new BinaryTree<String>("Ligma", null, null);
         BinaryTree root = new BinaryTree<String>("Loool", links, rechts);
 
@@ -37,6 +50,7 @@ public class Main extends JPanel {
             
             Main drawingPanel = new Main();
             frame.add(drawingPanel);
+            drawTree(root, frame, 400, 50);
 
             // Set the size of the window
             frame.setSize(400, 300);
