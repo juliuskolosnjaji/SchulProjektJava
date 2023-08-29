14bd9deedc1825beb0b4a999097230dbd8442777
juliuskolosnjaji <82638084+juliuskolosnjaji@users.noreply.github.com>
2023-08-29 21:14:21 +0200

    update

--- Bäume/BinaryTree/MorseBaum.java
+++ Bäume/BinaryTree/MorseBaum.java
@@ -9,6 +9,15 @@ public class MorseBaum{
         BinaryTree<Character> hBaum = new BinaryTree<Character>(new Character('h'));
         BinaryTree<Character> vBaum = new BinaryTree<Character>(new Character('v'));
         BinaryTree<Character> sBaum = new BinaryTree<Character>(new Character('s'), hBaum, vBaum);
+        BinaryTree<Character> iBaum = new BinaryTree<Character>(new Character('i'), sBaum, uBaum);
+        BinaryTree<Character> fBaum = new BinaryTree<Character>(new Character('f'));
+        BinaryTree<Character> uBaum = new BinaryTree<Character>(new Character('u'), fBaum, null);
+        BinaryTree<Character> eBaum = new BinaryTree<Character>(new Character('e'), iBaum, aBaum);
+        BinaryTree<Character> aBaum = new BinaryTree<Character>(new Character('a'), rBaum, wBaum);
+        BinaryTree<Character> rBaum = new BinaryTree<Character>(new Character('r'), lBaum, null);
+        BinaryTree<Character> lBaum = new BinaryTree<Character>(new Character('l'));
+
+    
     }
 
     public String decode(String pCode){return pCode;}
