903909e8aa8b3cba5be320a54bc5142e32da126c
juliuskolosnjaji <82638084+juliuskolosnjaji@users.noreply.github.com>
2023-09-08 16:07:53 +0200

    pls fix

--- Baeume/Tree Traversal/TraverseTree.java
+++ Baeume/Tree Traversal/TraverseTree.java
@@ -3,43 +3,27 @@ public class TraverseTree{
     if (pTree == null) return "";
     
     if(pTree.getContent() != null) {
-      return 
-        pTree.getContent() +
-        preOrder(pTree.getLeftTree()) + 
-        preOrder(pTree.getRightTree());
+      return (pTree.getContent() + preOrder(pTree.getLeftTree()) + preOrder(pTree.getRightTree()));
     } else {
-      return
-        preOrder(pTree.getLeftTree()) + 
-        preOrder(pTree.getRightTree());
+      return (preOrder(pTree.getLeftTree()) + preOrder(pTree.getRightTree()));
     }
   }
 
   public String preOrder(pTree BinaryTree<Character>) {
-    if (pTree == null) return "";
-    
+    if (pTree == null){ return "";}
     if(pTree.getContent() != null) {
-      return 
-        pTree.getContent() +
-        preOrder(pTree.getLeftTree()) + 
-        preOrder(pTree.getRightTree());
+      return (pTree.getContent() + preOrder(pTree.getLeftTree()) + preOrder(pTree.getRightTree()));
     } else {
-      return
-        preOrder(pTree.getLeftTree()) + 
-        preOrder(pTree.getRightTree());
+      return (preOrder(pTree.getLeftTree()) + preOrder(pTree.getRightTree()));
     }
   }
 
   public String postOrder(pTree BinaryTree<Character>) {
-    if (pTree == null) return "";
-    
+    if (pTree == null){ return "";}
+
     if(pTree.getContent() != null) {
-      return
-        preOrder(pTree.getLeftTree()) + 
-        preOrder(pTree.getRightTree()) +
-        pTree.getContent();
+      return (preOrder(pTree.getLeftTree()) + preOrder(pTree.getRightTree()) + pTree.getContent());
     } else {
-      return
-        preOrder(pTree.getLeftTree()) + 
-        preOrder(pTree.getRightTree());
+      return (preOrder(pTree.getLeftTree()) + preOrder(pTree.getRightTree()));
     }
 }
